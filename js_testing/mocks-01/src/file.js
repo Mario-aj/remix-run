b734d4e79bc6866fa1563382e02c9307c145d79a
mario-aj <mariojorge1997@mail.ru>
2022-10-26 14:26:09 +0100

    doing our first validation

--- js_testing/mocks-01/src/file.js
+++ js_testing/mocks-01/src/file.js
@@ -1,9 +1,22 @@
 const { readFile } = require('fs/promises');
 const { join } = require('path');
 
+const { error } = require('./constants');
+
+const DEFAULT_OPTIONS = {
+  maxLines: 3,
+  fields: ['id', 'name', 'profession', 'age'],
+};
 class File {
   static async csvToJson(filePath) {
     const content = await this.getFileContent(filePath);
+
+    const validate = this.isValid(content);
+
+    if (!validate.valid) {
+      throw Error(validate.error);
+    }
+
     return content;
   }
 
@@ -12,9 +25,22 @@ class File {
 
     return (await readFile(filename)).toString('utf-8');
   }
+
+  static isValid(csvString, options = DEFAULT_OPTIONS) {
+    const [header, ...fileWithoutHeader] = csvString.split('\n');
+    const isHeaderValid = header === options.fields.join(',');
+
+    if (!isHeaderValid) {
+      return {
+        error: error.FILE_FIELDS_ERROR_MESSAGE,
+        valid: false,
+      };
+    }
+  }
 }
 
 (async () => {
-  const result = await File.csvToJson('../mocks/threeItems-valid.csv');
+  // const result = await File.csvToJson('../mocks/threeItems-valid.csv');
+  const result = await File.csvToJson('../mocks/invalid-header.csv');
   console.log(result);
 })();
