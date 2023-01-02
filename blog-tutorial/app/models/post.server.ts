b35c11688e282b998f7b206e063c50e93926182e
mario-aj <mariojorge1997@mail.ru>
2023-01-02 09:25:30 +0100

    rendering post by slug

--- blog-tutorial/app/models/post.server.ts
+++ blog-tutorial/app/models/post.server.ts
@@ -3,3 +3,7 @@ import { prisma } from "~/db.server";
 export async function getPosts() {
   return prisma.post.findMany();
 }
+
+export async function getPost(slug: string) {
+  return prisma.post.findUnique({ where: { slug } });
+}
