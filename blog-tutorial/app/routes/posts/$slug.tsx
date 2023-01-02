0301059095a2e9722b328a15595b7ecfcca7968e
mario-aj <mariojorge1997@mail.ru>
2023-01-02 09:34:58 +0100

    using marked to render markdown content

--- blog-tutorial/app/routes/posts/$slug.tsx
+++ blog-tutorial/app/routes/posts/$slug.tsx
@@ -1,23 +1,30 @@
 import { json } from "@remix-run/node";
 import type { LoaderArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+import { marked } from "marked";
 
 import { getPost } from "~/models/post.server";
 
 export const loader = async ({ params }: LoaderArgs) => {
-  const post = await getPost(params.slug as string);
+  invariant(params.slug, "params.slug is required");
+  const post = await getPost(params.slug);
+  invariant(post, `Post not found: ${params.slug}`);
 
-  return json({ post });
+  const html = marked(post.markdown);
+
+  return json({ post, html });
 };
 
 export default function PostSlug() {
-  const { post } = useLoaderData<typeof loader>();
+  const { post, html } = useLoaderData<typeof loader>();
 
   return (
     <main className="mx-auto max-w-4xl">
       <h1 className="my-6 border-b-2 text-center text-3xl">
-        Some Post: {post?.title}
+        Some Post: {post.title}
       </h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
   );
 }
